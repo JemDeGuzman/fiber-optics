7c69c935c76cd82b52f2751601296e2252d2751b
JemDeGuzman <qjedeguzman03@tip.edu.ph>
2026-02-16 20:35:28 +0800

    prisma change

--- services/api/src/lib/prisma.ts
+++ services/api/src/lib/prisma.ts
@@ -1,6 +1,6 @@
 
 // services/api/src/lib/prisma.ts
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '../../node_modules/.prisma/client';
 
 const prisma = new PrismaClient();
 
